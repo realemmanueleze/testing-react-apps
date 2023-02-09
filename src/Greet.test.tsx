848e62a48fec714fdc2fd91a71d360edbe88a81a
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2023-02-09 01:22:03 +0100

    feat: introduced Jest' describe method for grouping tests

--- src/Greet.test.tsx
+++ src/Greet.test.tsx
@@ -1,14 +1,15 @@
 import { screen, render } from "@testing-library/react";
 import Greet from "./Greet";
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const text = screen.getByText(/Hello/i);
+    expect(text).toBeInTheDocument();
+  });
 
-test("Greet renders properly", () => {
-  render(<Greet />);
-  const text = screen.getByText(/Hello/i);
-  expect(text).toBeInTheDocument();
-});
-
-test.skip("Greet renders renders with props", () => {
-  render(<Greet name="Joy"/>);
-  const text = screen.getByText(/Hello Joy/i);
-  expect(text).toBeInTheDocument();
+  test("render name", () => {
+    render(<Greet name="Joy" />);
+    const text = screen.getByText(/Hello Joy/i);
+    expect(text).toBeInTheDocument();
+  });
 });
